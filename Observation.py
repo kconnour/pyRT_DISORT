acaa2ac93589b6ceda5947fd0ae037f69a0cb311
kyle <kdconey1@gmail.com>
2020-08-14 18:21:34 -0600

    Added the Observation class to cover quantities the observer is responsible for providing

---
+++ Observation.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+class Observation:
+    def __init__(self, short_wavelength, long_wavelength, solar_zenith_angle, emission_angle, phase_angle, latitude,
+                 longitude):
+        """ Initialize the class
+
+        Parameters
+        ----------
+        short_wavelength: float
+            The shorter wavelength for this pixel (in nm)
+        long_wavelength: float
+            The longer wavelength for this pixel (in nm)
+        solar_zenith_angle: float
+            The pixel solar zenith angle (in degrees)
+        emission_angle: float
+            The pixel emission angle (in degrees)
+        phase_angle: float
+            The pixel phase angle (in degrees)
+        latitude: float
+            The pixel latitude (in degrees)
+        longitude: float
+            The pixel longitude (in degrees east). Note The convention is 0--360, not -180 -- +180
+        """
+        self.short_wavelength = short_wavelength
+        self.long_wavelength = long_wavelength
+        self.sza = solar_zenith_angle
+        self.emission = emission_angle
+        self.phase = phase_angle
+        self.latitude = latitude
+        self.longitude = longitude
+        self.phi0 = 0
+        self.low_wavenumber = self.wavelength_to_wavenumber(self.long_wavelength)
+        self.high_wavenumber = self.wavelength_to_wavenumber(self.short_wavelength)
+
+    def mu0(self):
+        """ Calculate the cosine of the solar zenith angle
+
+        Returns
+        -------
+        mu0 = cos(theta_0)
+        """
+        return np.cos(np.deg2rad(self.sza))
+
+    def mu(self):
+        """ Calculate the cosine of the emission angle
+
+        Returns
+        -------
+        mu = cos(theta)
+        """
+        return np.cos(np.deg2rad(self.emission))
+
+    def phi(self):
+        """ Calculate the azimuthal angle for the given geometry.
+
+        Returns
+        -------
+        azimuthal_angle: float
+            The angle in degrees
+        """
+
+        # I used a different calculation than Mike's.. but they seem to give the same answer and this is faster
+        sin_emission_angle = np.cos(np.deg2rad(90-self.emission))
+        sin_solar_zenith_angle = np.cos(np.deg2rad(90-self.sza))
+
+        # Trap the case of the emission angle or solar zenith angle = 0
+        if sin_emission_angle == 0 or sin_solar_zenith_angle == 0:
+            d_phi = np.pi
+
+        else:
+            temp_var = (np.cos(np.deg2rad(self.phase)) - self.mu0()*self.mu()) / \
+                       (sin_emission_angle * sin_solar_zenith_angle)
+            # Trap the round-off case for arc cosine
+            if np.abs(temp_var) > 1:
+                temp_var = np.sign(temp_var)
+            d_phi = np.arccos(temp_var)
+
+        d_phi = np.rad2deg(d_phi)
+        azimuthal_angle = self.phi0 + 180 - d_phi
+        return azimuthal_angle
+
+    def wavelength_to_wavenumber(self, wavelength):
+        """ Convert wavelength (in nm) to wavenumber (in inverse cm)
+
+        Returns
+        -------
+        wavenumber: float
+            The wavenumber for this observation
+        """
+        cm_wavelength = wavelength * 10**-7
+        return 1 / cm_wavelength
