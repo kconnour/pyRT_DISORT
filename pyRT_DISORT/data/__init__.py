864476cce52511bd0e7f46d88e3ddaa9485dd840
kyle <kdconey1@gmail.com>
2020-10-19 17:25:09 -0600

    Make a better installation

--- pyRT_DISORT/data/__init__.py
+++ pyRT_DISORT/data/__init__.py
@@ -1,1 +1,3 @@
 from .get_data import get_data_path
+from . import aux
+from . import planets
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import setuptools
 import sys
@@ -9,23 +10,28 @@ def compile_disort():
     module_name = 'disort'
     os.chdir(os.path.join(repository_path, 'disort4.0.98/'))
     os.system('{} -m numpy.f2py -c BDREF.f DISOBRDF.f DISORT.f ERRPACK.f LAPACK.f LINPAK.f RDI1MACH.f -m {}'.format(python, module_name))
+    file = glob.glob('*.so')[0]
+    os.rename('{}{}'.format(os.path.join(repository_path, 'disort4.0.98/'), file), '{}/{}'.format(repository_path, file))
+    os.chdir(repository_path)
+    return file
 
 
-def setup_pyRT_DISORT():
+def setup_pyRT_DISORT(file):
     setuptools.setup(
         name='pyRT_DISORT',
         version='0.0.1',
         author='kconnour',
         description='This does RT',
         packages=setuptools.find_packages(),
+        #include_package_data=False,
         include_package_data=True,
         python_requires='>=3.8',
         install_requires=[
             'numpy>=1.19.1'
         ],
-        package_data={'': ['disort*.so']}
+        package_data={'': ['../{}'.format(file)]}
     )
 
 
-compile_disort()
-setup_pyRT_DISORT()
+file = compile_disort()
+setup_pyRT_DISORT(file)
