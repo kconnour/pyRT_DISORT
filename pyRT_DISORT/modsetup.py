20ac8caf89870c6ac10ee8b367d3cee88273f400
kyle <kdconey1@gmail.com>
2021-01-28 15:50:11 -0700

    Add Output class

--- pyRT_DISORT/modsetup.py
+++ pyRT_DISORT/modsetup.py
@@ -1,7 +1,197 @@
-"""modsetup.py contains structures for creating DISORT inputs not related to
-the physical model---things like creating output arrays, setting the number of
-streams, etc.
+"""modsetup.py contains data structures for creating DISORT inputs not related
+to atmospheric radiative properties.
 """
+import numpy as np
+
+
+class Output:
+    """Output is a data structure to hold the output arrays required by DISORT.
+
+    Output creates all 8 arrays that get populated with values as DISORT is run.
+
+    """
+
+    def __init__(self, size) -> None:
+        """
+        Parameters
+        ----------
+        size: Size
+            Data structure holding the model computational variables.
+
+        Raises
+        ------
+        TypeError
+            Raised if input is not an instance of Size.
+
+        """
+        self.__size = size
+
+        self.__albedo_medium = self.__make_albedo_medium()
+        self.__diffuse_up_flux = self.__make_diffuse_up_flux()
+        self.__diffuse_down_flux = self.__make_diffuse_down_flux()
+        self.__direct_beam_flux = self.__make_direct_beam_flux()
+        self.__flux_divergence = self.__make_flux_divergence()
+        self.__intensity = self.__make_intensity()
+        self.__mean_intensity = self.__make_mean_intensity()
+        self.__transmissivity_medium = self.__make_transmissivity_medium()
+
+    def __raise_type_error_if_input_is_not_size(self) -> None:
+        if not isinstance(self.__size, Size):
+            raise TypeError('size must be an instance of Size.')
+
+    def __make_albedo_medium(self) -> np.ndarray:
+        return np.zeros(self.__size.n_umu)
+
+    def __make_diffuse_up_flux(self) -> np.ndarray:
+        return np.zeros(self.__size.n_user_levels)
+
+    def __make_diffuse_down_flux(self) -> np.ndarray:
+        return np.zeros(self.__size.n_user_levels)
+
+    def __make_direct_beam_flux(self) -> np.ndarray:
+        return np.zeros(self.__size.n_user_levels)
+
+    def __make_flux_divergence(self) -> np.ndarray:
+        return np.zeros(self.__size.n_user_levels)
+
+    def __make_intensity(self) -> np.ndarray:
+        return np.zeros((self.__size.n_umu, self.__size.n_user_levels,
+                         self.__size.n_phi))
+
+    def __make_mean_intensity(self) -> np.ndarray:
+        return np.zeros(self.__size.n_user_levels)
+
+    def __make_transmissivity_medium(self) -> np.ndarray:
+        return np.zeros(self.__size.n_umu)
+
+    @property
+    def albedo_medium(self) -> np.ndarray:
+        """Get the albedo of the medium output array.
+
+        Returns
+        -------
+        np.ndarray
+            The albedo of the medium.
+
+        Notes
+        -----
+        In DISORT, this variable is named "ALBMED".
+
+        """
+        return self.__albedo_medium
+
+    @property
+    def diffuse_up_flux(self) -> np.ndarray:
+        """Get the diffuse upward flux output array.
+
+        Returns
+        -------
+        np.ndarray
+            The diffuse upward flux.
+
+        Notes
+        -----
+        In DISORT, this variable is named "FLUP".
+
+        """
+        return self.__diffuse_up_flux
+
+    @property
+    def diffuse_down_flux(self) -> np.ndarray:
+        """Get the diffuse downward flux output array.
+
+        Returns
+        -------
+        np.ndarray
+            The diffuse downward flux.
+
+        Notes
+        -----
+        In DISORT, this variable is named "RFLDN" (total minus direct-beam).
+
+        """
+        return self.__diffuse_down_flux
+
+    @property
+    def direct_beam_flux(self) -> np.ndarray:
+        """Get the direct beam flux output array.
+
+        Returns
+        -------
+        np.ndarray
+            The direct beam flux.
+
+        Notes
+        -----
+        In DISORT, this variable is named "RFLDIR".
+
+        """
+        return self.__direct_beam_flux
+
+    @property
+    def flux_divergence(self) -> np.ndarray:
+        """Get the flux divergence output array.
+
+        Returns
+        -------
+        np.ndarray
+            The flux divergence.
+
+        Notes
+        -----
+        In DISORT, this variable is named "DFDT", which is
+        (d(net_flux) / d(optical_depth)). This is an exact result.
+
+        """
+        return self.__flux_divergence
+
+    @property
+    def intensity(self) -> np.ndarray:
+        """Get the intensity output array.
+
+        Returns
+        -------
+        np.ndarray
+            The intensity.
+
+        Notes
+        -----
+        In DISORT, this variable is named "UU".
+
+        """
+        return self.__intensity
+
+    @property
+    def mean_intensity(self) -> np.ndarray:
+        """Get the mean intensity output array.
+
+        Returns
+        -------
+        np.ndarray
+            The mean intensity.
+
+        Notes
+        -----
+        In DISORT, this variable is named "UAVG".
+
+        """
+        return self.__mean_intensity
+
+    @property
+    def transmissivity_medium(self) -> np.ndarray:
+        """Get the transmissivity of the medium output array.
+
+        Returns
+        -------
+        np.ndarray
+            The transmissivity of the medium.
+
+        Notes
+        -----
+        In DISORT, this variable is named "TRNMED".
+
+        """
+        return self.__transmissivity_medium
 
 
 class Size:
@@ -179,3 +369,18 @@ class Size:
 
         """
         return self.__n_user_levels
+
+
+# TODO: Become sure of what this does and remove it
+class Unsure:
+    """This class makes the variable "h_lyr". I don't really know what this
+    variable does since it can be all 0s.
+
+    """
+    def __init__(self, size):
+        self.size = size
+        self.__h_lyr = np.zeros(self.size.n_layers+1)
+
+    @property
+    def h_lyr(self):
+        return self.__h_lyr
