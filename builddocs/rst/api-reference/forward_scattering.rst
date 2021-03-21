95b137f6375d8a2f7bbeedd53ebd7ee30c05dc10
kyle <kdconey1@gmail.com>
2021-03-21 16:57:40 -0600

    Add NN forward scattering properties

---
+++ builddocs/rst/api-reference/forward_scattering.rst
@@ -0,0 +1,7 @@
+Forward Scattering
+==================
+
+.. automodule:: forward_scattering
+   :members:
+   :exclude-members: ForwardScatteringProperty
+   :special-members: __init__
--- pyRT_DISORT/forward_scattering.py
+++ pyRT_DISORT/forward_scattering.py
@@ -1,79 +1,107 @@
 import numpy as np
 
 
-# TODO: Include an optional g input parameter
-class ForwardScatteringProperties:
+class ForwardScatteringProperty:
+    """An abstract class to check something is a forward scattering property.
+
+    """
+
+    def __init__(self, parameter: np.ndarray, particle_size: np.ndarray,
+                 wavelength: np.ndarray, name: str) -> None:
+        self.__parameter = parameter
+        self.__particle_size = particle_size
+        self.__wavelength = wavelength
+        self.__name = name
+
+        self.__raise_error_if_inputs_are_bad()
+
+    def __raise_error_if_inputs_are_bad(self) -> None:
+        self.__raise_type_error_if_parameter_is_not_ndarray()
+        self.__raise_value_error_if_shapes_do_not_match()
+
+    def __raise_type_error_if_parameter_is_not_ndarray(self) -> None:
+        if not isinstance(self.__parameter, np.ndarray):
+            message = f'{self.__name} must be a numpy.ndarray.'
+            raise TypeError(message)
+
+    def __raise_value_error_if_shapes_do_not_match(self) -> None:
+        if np.ndim(self.__parameter) != 2:
+            message = f'{self.__name} must be a 2D array.'
+            raise ValueError(message)
+
+        if self.__parameter.shape[0] != self.__particle_size.shape[0]:
+            raise ValueError('parameter does not match particle size shape')
+
+        if self.__parameter.shape[1] != self.__wavelength.shape[0]:
+            raise ValueError('parameter does not match wavelength shape.')
+
+    @property
+    def parameter(self) -> np.ndarray:
+        return self.__parameter
+
+    @property
+    def particle_size(self) -> np.ndarray:
+        return self.__particle_size
+
+    @property
+    def wavelength(self) -> np.ndarray:
+        return self.__wavelength
+
+
+class NearestNeighborSingleScatteringAlbedo:
+    """A class to make the single scattering albedo related variables.
+
+    NearestNeighborSingleScatteringAlbedo accepts the scattering and extinction
+    cross sections, along with a 1D wavelength and particle size grid. It then
+    finds the nearest neighbor value in the input forward scattering properties
+    to the grids, and creates the properties on a particle size and wavelength
+    grid.
+
+    """
     def __init__(self, scattering_cross_section: np.ndarray,
                  extinction_cross_section: np.ndarray,
-                 particle_size_grid: np.ndarray, wavelength_grid) -> None:
+                 particle_size_grid: np.ndarray, wavelength_grid: np.ndarray,
+                 particle_size: np.ndarray,
+                 wavelength: np.ndarray) -> None:
         """
         Parameters
         ----------
         scattering_cross_section
-            2D grid of the scattering cross sections. This is assumed to have
-            shape [particle_size_grid, wavelength_grid].
+            2D array of the scattering cross section (the 0th axis is assumed to
+            be the particle size axis, while the 1st axis is assumed to be the
+            wavelength axis).
         extinction_cross_section
-            2D grid of the extinction cross sections. This is assumed to have
-            shape [particle_size_grid, wavelength_grid].
+            2D array of the extinction cross section with same dims as above.
         particle_size_grid
-            1D grid of the particle sizes.
+            1D array of particle sizes over which the above properties are
+            defined.
         wavelength_grid
-            1D grid of the wavelengths.
+            1D array of wavelengths over which the above properties are defined.
+        particle_size
+            1D array of particle sizes to regrid the properties onto.
+        wavelength
+            1D array of wavelengths to regrid the properties onto.
 
         """
+        self.__c_sca = ForwardScatteringProperty(scattering_cross_section,
+                                                 particle_size_grid,
+                                  wavelength_grid, 'scattering')
+        self.__c_ext = ForwardScatteringProperty(extinction_cross_section,
+                                                 particle_size_grid,
+                                  wavelength_grid, 'extinction')
+        self.__particle_size = particle_size
+        self.__wavelength = wavelength
 
-        self.__scattering = scattering_cross_section
-        self.__extinction = extinction_cross_section
-        self.__sizes = particle_size_grid
-        self.__wavelength = wavelength_grid
-        self.__ssa = self.__make_single_scattering_albedo()
-
-    def __make_single_scattering_albedo(self) -> np.ndarray:
-        try:
-            return self.__scattering / self.__extinction
-        except ValueError as ve:
-            raise ValueError('The scattering and extinction arrays cannot be'
-                             'broadcast together.') from ve
-
-    @property
-    def scattering_cross_section(self) -> np.ndarray:
-        return self.__scattering
-
-    @property
-    def extinction_cross_section(self) -> np.ndarray:
-        return self.__extinction
-
-    @property
-    def particle_size_grid(self) -> np.ndarray:
-        return self.__sizes
-
-    @property
-    def wavelength_grid(self) -> np.ndarray:
-        return self.__wavelength
-
-    @property
-    def single_scattering_albedo(self) -> np.ndarray:
-        return self.__ssa
-
-
-class GriddedForwardScatteringProperties:
-    def __init__(self,
-                 forward_scattering_properties: ForwardScatteringProperties,
-                 particle_size_profile: np.ndarray,
-                 wavelengths: np.ndarray, reference_wavelength: float) -> None:
-        self.__fsp = forward_scattering_properties
-        self.__size_inds, self.__wavelength_inds = \
-            self.__get_nearest_neighbor_values(particle_size_profile, wavelengths)
+        self.__nnps, self.__nnw = self.__get_nearest_neighbor_values()
         self.__gridded_c_scattering = self.__make_gridded_c_scattering()
         self.__gridded_c_extinction = self.__make_gridded_c_extinction()
-        self.__gridded_ssa = self.__make_gridded_single_scattering_albedo()
-        self.__extinction = self.__make_extinction_grid(reference_wavelength)
+        self.__gridded_ssa = self.__make_gridded_ssa()
 
-    def __get_nearest_neighbor_values(self, particle_size_profile, wavelengths):
-        size_indices = self.__get_nearest_indices(self.__fsp.particle_size_grid,
-                                                  particle_size_profile)
+    def __get_nearest_neighbor_values(self):
+        size_indices = self.__get_nearest_indices(
+            self.__c_sca.particle_size, self.__particle_size)
         wavelength_indices = self.__get_nearest_indices(
-            self.__fsp.wavelength_grid, wavelengths)
+            self.__c_sca.wavelength, self.__wavelength)
         return size_indices, wavelength_indices
 
     @staticmethod
@@ -82,32 +110,45 @@ class GriddedForwardScatteringProperties:
         # grid should be 1D; values can be ND
         return np.abs(np.subtract.outer(grid, values)).argmin(0)
 
-    def __make_gridded_c_scattering(self) -> np.ndarray:
-        return self.__fsp.scattering_cross_section[self.__size_inds, :][:, self.__wavelength_inds]
+    def __make_gridded_c_scattering(self):
+        return self.__c_sca.parameter[self.__nnps, :][:, self.__nnw]
 
-    def __make_gridded_c_extinction(self) -> np.ndarray:
-        return self.__fsp.extinction_cross_section[self.__size_inds, :][:, self.__wavelength_inds]
+    def __make_gridded_c_extinction(self):
+        return self.__c_ext.parameter[self.__nnps, :][:, self.__nnw]
 
-    def __make_gridded_single_scattering_albedo(self) -> np.ndarray:
+    def __make_gridded_ssa(self):
         return self.__gridded_c_scattering / self.__gridded_c_extinction
-
-    def __make_extinction_grid(self, reference_wavelength) -> np.ndarray:
-        size_ind, wav_ind = self.__get_nearest_neighbor_values(self.__fsp.particle_size_grid, np.array([reference_wavelength]))
-        extinction_profile = self.__fsp.extinction_cross_section[self.__size_inds, wav_ind]
-        return (self.__gridded_c_extinction.T / extinction_profile).T
 
     @property
     def scattering_cross_section(self) -> np.ndarray:
+        """Get the scattering cross section on the new grid.
+
+        """
         return self.__gridded_c_scattering
 
     @property
     def extinction_cross_section(self) -> np.ndarray:
+        """Get the extinction cross section on the new grid.
+
+        """
         return self.__gridded_c_extinction
 
     @property
     def single_scattering_albedo(self) -> np.ndarray:
+        """Get the single scattering albedo on the new grid.
+
+        """
         return self.__gridded_ssa
 
-    @property
-    def extinction(self) -> np.ndarray:
-        return self.__extinction
+    def make_extinction_grid(self, reference_wavelength: float) -> np.ndarray:
+        """Make a grid of the extinction cross section at a reference wavelength.
+        This is the extinction cross section at the input wavelengths divided by
+        the extinction cross section at the reference wavelength.
+
+        """
+        size_ind = self.__get_nearest_indices(self.__c_ext.particle_size,
+                                              self.__particle_size)
+        wav_ind = self.__get_nearest_indices(self.__c_ext.wavelength,
+                                             np.array([reference_wavelength]))
+        extinction_profile = self.__c_ext.parameter[size_ind, wav_ind]
+        return (self.__gridded_c_extinction.T / extinction_profile).T
