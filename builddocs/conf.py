c473a1ba8cb683de309ddeef4d1feb2f53570b50
kyle <kdconey1@gmail.com>
2021-08-09 15:50:34 -0600

    Change doc theme to pydata's sphinx theme
    Update setup.cfg to include a docs section

--- builddocs/conf.py
+++ builddocs/conf.py
@@ -3,8 +3,8 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import sphinx_rtd_theme
-import matplotlib
+#import pydata_sphinx_theme
+#import matplotlib
 
 # -- Path setup --------------------------------------------------------------
 
@@ -51,8 +51,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+    "github_url": "https://github.com/kconnour/pyRT_DISORT",
+    "collapse_navigation": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
