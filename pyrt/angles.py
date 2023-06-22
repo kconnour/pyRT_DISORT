eddfec7c3d8016687d1c44228de94905228ebc4f
Kyle <kyle.d.connour@gmail.com>
2023-06-22 17:58:17 -0600

    Add additional azimuth docs and tests

--- pyrt/angles.py
+++ pyrt/angles.py
@@ -4,10 +4,8 @@ import numpy as np
 from numpy.typing import ArrayLike
 
 
-def azimuth(incidence_angles: ArrayLike,
-            emission_angles: ArrayLike,
-            phase_angles: ArrayLike) \
-        -> np.ndarray:
+def azimuth(incidence_angles: ArrayLike, emission_angles: ArrayLike,
+            phase_angles: ArrayLike) -> np.ndarray:
     r"""Construct azimuth angles from a set of incidence, emission, and phase
     angles.
 
@@ -28,25 +26,40 @@ def azimuth(incidence_angles: ArrayLike,
     np.ndarray
         N-dimensional array of azimuth angles [degrees].
 
+    Notes
+    -----
+    This function can handle arrays of any shape, but all input arrays should
+    have the same shape.
+
     Examples
     --------
-    Create the azimuth angles from a set of angles.
+    Create the azimuth angle from a scalar set of angles.
 
     >>> import numpy as np
     >>> import pyrt
-    >>> incidence = np.array([10, 20])
-    >>> emission = np.array([15, 25])
-    >>> phase = np.array([20, 30])
-    >>> pyrt.azimuth(incidence, emission, phase)
-    array([75.09711684, 95.70740729])
+    >>> pyrt.azimuth(10, 15, 20)
+    array(75.09711684)
+
+    You can also input an N-dimensional set of angles. This may be useful for
+    spacecraft observations, where each pixel in a 2D image has a unique set
+    of incidence, emission, and phase angles.
+
+    >>> data_shape = (15, 20)
+    >>> incidence = np.ones(data_shape) * 10
+    >>> emission = np.ones(data_shape) * 15
+    >>> phase = np.ones(data_shape) * 20
+    >>> pyrt.azimuth(incidence, emission, phase).shape
+    (15, 20)
 
     """
     with np.errstate(divide='ignore', invalid='ignore'):
         tmp_arg = np.true_divide(
-            np.cos(np.radians(phase_angles)) - np.cos(np.radians(emission_angles)) *
+            np.cos(np.radians(phase_angles)) -
+            np.cos(np.radians(emission_angles)) *
             np.cos(np.radians(incidence_angles)),
-            np.sin(np.radians(emission_angles)) * np.sin(np.radians(incidence_angles)))
-        tmp_arg = np.asarray(tmp_arg)
-        tmp_arg[~np.isfinite(tmp_arg)] = -1
-        d_phi = np.arccos(np.clip(tmp_arg, -1, 1))
+            np.sin(np.radians(emission_angles)) *
+            np.sin(np.radians(incidence_angles)))
+    tmp_arg = np.asarray(tmp_arg)
+    tmp_arg[~np.isfinite(tmp_arg)] = -1
+    d_phi = np.arccos(np.clip(tmp_arg, -1, 1))
     return np.array(180 - np.degrees(d_phi))
